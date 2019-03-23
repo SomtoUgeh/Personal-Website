8db602b5ae881b5a1e834c7c63797d4223c1d024
Somto M.Ugeh <smugeh@gmail.com>
2019-03-23 21:14:58 +0100

    Set up homepage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import HomePage from './components/HomePage';
 
 class App extends Component {
   render() {
-    return <p>hi</p>;
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+        </Switch>
+      </BrowserRouter>
+    );
   }
 }
 
